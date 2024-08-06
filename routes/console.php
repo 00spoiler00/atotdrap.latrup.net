cf56ecef8a344a73a0327fb8eb16054db07f6308
Marc Boada <marc@latrup.net>
2024-08-06 14:03:26 +0200

    Update schedules

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Console\Commands\UpdateDrivers;
+use App\Console\Commands\UpdateRegistrations;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command(UpdateDrivers::class)->everyFiveMinutes()->withoutOverlapping()->runInBackground();
+Schedule::command(UpdateRegistrations::class)->everyFiveMinutes()->withoutOverlapping()->runInBackground();
