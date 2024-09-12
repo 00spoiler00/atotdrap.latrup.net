37ed9b9637c31a52763020fddfff75ee3f67653c
Marc Boada <marc@latrup.net>
2024-09-12 17:53:40 +0200

    Refactor user route to use arrow function syntax

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware(['auth:sanctum'])->get('/user', fn (Request $request) => $request->user());
 
 Route::get('/frontVersion', fn () => response()->json(['version' => '1.0.0']));
 Route::get('/dashboard', 'App\Http\Controllers\GenericController@dashboard');
